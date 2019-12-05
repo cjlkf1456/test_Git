482cac3aacd1c8991208f80387bbcb91fddc4316
cjlkf1456 <58247408+cjlkf1456@users.noreply.github.com>
2019-12-05 20:09:15 +0800

    Update GitDemo.java

--- src/main/java/com/magickey/controller/GitDemo.java
+++ src/main/java/com/magickey/controller/GitDemo.java
@@ -2,6 +2,7 @@ package com.magickey.controller;
 
 public class GitDemo {
     public static void main(String[] args) {
+        System.out.println("Hello!!");
         System.out.println("Hello Git!!");
     }
 }
